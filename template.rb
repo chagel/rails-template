55298861ebe4b3fd106283e7300f2103870e4331
Mike Chen <chagel@gmail.com>
2015-12-28 23:44:08 +0800

    update source paths

--- template.rb
+++ template.rb
@@ -1,19 +1,37 @@
-source_paths.unshift(File.dirname(__FILE__))
+require "fileutils"
+require "shellwords"
 
-copy_file "Gemfile", force: true
-copy_file "Capfile"
-copy_file "Guardfile"
-copy_file '.rspec'
-copy_file 'spec/rails_helper.rb'
-copy_file 'spec/spec_helper.rb'
-copy_file 'config/sidekiq.yml'
-copy_file 'config/deploy.rb'
-copy_file 'config/deploy/production.rb'
-copy_file 'config/deploy/staging.rb'
+def bootstrap
+  prepare 
+  copy_file "Gemfile", force: true
+  copy_file "Capfile"
+  copy_file "Guardfile"
+  copy_file '.rspec'
+  copy_file 'spec/rails_helper.rb'
+  copy_file 'spec/spec_helper.rb'
+  copy_file 'config/sidekiq.yml'
+  copy_file 'config/deploy.rb'
+  copy_file 'config/deploy/production.rb'
+  copy_file 'config/deploy/staging.rb'
+  remove_file "README.rdoc"
+  copy_file "README.md"
 
-remove_file "README.rdoc"
-copy_file "README.md"
+  run("bundle install") 
+  git :init
+end
 
-run("bundle install") 
-git :init
+def prepare
+  if __FILE__ =~ /^https?:\/\//
+    tempdir = "/tmp/rails-template"
+    source_paths.unshift(tempdir)
+    git :clone => [
+      "--quiet",
+      "https://github.com/chagel/rails-template.git",
+      tempdir
+    ].join(" ")
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
 
+bootstrap
