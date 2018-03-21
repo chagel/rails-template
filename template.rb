13dd22f0b3632d5fba08d76a94a4e18cb68ada20
Mike Chen <chagel@gmail.com>
2018-03-21 23:50:49 +0800

    update to 5.1.5

--- template.rb
+++ template.rb
@@ -4,15 +4,14 @@ require "shellwords"
 def bootstrap
   prepare 
   copy_file "Gemfile", force: true
-  copy_file "Capfile"
+  copy_file "Gemfile.lock", force: true
   copy_file "Guardfile"
   copy_file '.rspec'
+  copy_file '.ruby-version'
   copy_file 'spec/rails_helper.rb'
   copy_file 'spec/spec_helper.rb'
   copy_file 'config/sidekiq.yml'
   copy_file 'config/deploy.rb'
-  copy_file 'config/deploy/production.rb'
-  copy_file 'config/deploy/staging.rb'
   copy_file 'config/database.yml', force: true
   copy_file "README.md", force: true
 
